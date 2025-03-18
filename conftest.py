4f4e9470e6441dcd3d80b712497a6ce45d373ebd
Soumendra-prasad <soumendra@gocodeo.com>
2025-03-18 12:02:21 +0530

    initial commit

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ import sys
 from browser_use.logging_config import setup_logging
 
 # Get the absolute path to the project root
+# Get the absolute path to the project root1
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, project_root)
 
