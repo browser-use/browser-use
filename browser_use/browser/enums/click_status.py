4fb433e5527da9cf2be60ba7df715b2d66385d43
PaperBoardOfficial <hiremohitforsoftwarerole@gmail.com>
2025-03-26 11:27:15 +0530

    cosmetic refactoring

--- browser_use/browser/enums/click_status.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class ClickStatus(Enum):
-    SUCCESS = "success"
-    NAVIGATION_SUCCESS = "navigation_success"
-    ERROR = "error"
-    DOWNLOAD_SUCCESS = "download_success"
-    NAVIGATION_DISALLOWED = "navigation_disallowed"
