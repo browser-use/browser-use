f20e50b0ffbeb6b604eaec2e6c38c18eed40018b
Anish Hiranandani <anish.hira@gmail.com>
2025-03-27 17:47:01 -0700

    temp commit on ctrl+c bahavior

--- examples/browser/real_browser.py
+++ examples/browser/real_browser.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import logging
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import asyncio
@@ -11,15 +12,18 @@ from browser_use import Agent, Browser, BrowserConfig
 
 dotenv.load_dotenv()
 
+logger = logging.getLogger(__name__)
+
 browser = Browser(
 	config=BrowserConfig(
+          headless=False
 		# NOTE: you need to close your chrome browser - so that this can open your browser in debug mode
-		browser_binary_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+		#browser_binary_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
 	)
 )
 
 
-async def main():
+async def run_agent():
 	agent = Agent(
 		task='In docs.google.com write my Papa a quick letter',
 		llm=ChatOpenAI(model='gpt-4o'),
@@ -32,5 +36,19 @@ async def main():
 	input('Press Enter to close...')
 
 
+def main():   
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        task = loop.create_task(run_agent())
+        loop.run_until_complete(task)
+
+    except KeyboardInterrupt:
+        logger.info("Aborted!")
+        task.cancel()
+
+    finally:
+        loop.close()
+
 if __name__ == '__main__':
-	asyncio.run(main())
+	main()
