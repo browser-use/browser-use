8f88e45c375e5cd9f39c79a6029ad1e1c0010f46
PaperBoardOfficial <hiremohitforsoftwarerole@gmail.com>
2025-03-31 16:29:19 +0530

    wip

--- examples/browser/extension_browser.py
+++ examples/browser/extension_browser.py
@@ -18,7 +18,10 @@ llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api
 
 browser = Browser(
 	config=BrowserConfig(
-		extensions=[ExtensionConfig(name='uBlock Origin', extension_id='cjpalhdlnbpafiamejdnhcphjbkeiagm')]
+		extensions=[
+			ExtensionConfig(name='UBlock Origin Lite', extension_id='ddkjiahejlhfcafbddmgiahcphecmpfh'),
+			ExtensionConfig(name='Dark Reader', extension_id='eimadpbcbfnmbkopoojfekhnkhdbieeh'),
+		]
 	)
 )
 
