ab04e4629aba5a0a8e54090c5557bb36c0169b67
Ambar Choudhury <>
2025-04-15 12:14:38 -0700

    experimental - do not add a new page

--- browser_use/browser/context.py
+++ browser_use/browser/context.py
@@ -200,10 +200,14 @@ class BrowserContext:
 		logger.debug('Initializing browser context')
 
 		playwright_browser = await self.browser.get_playwright_browser()
-
 		context = await self._create_context(playwright_browser)
 		self._add_new_page_listener(context)
-		page = await context.new_page()
+		
+		# these two lines exist to not auto-open a new tab on starting a headed mode, with at least one pre-existing tab.
+		# the agent is able to open new tabs if needed.
+		# Check for existing pages before creating a new one
+		pages = context.pages
+		page = pages[0] if pages else await context.new_page()
 
 		# Instead of calling _update_state(), create an empty initial state
 		initial_state = self._get_initial_state(page)
