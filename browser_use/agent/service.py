d27e255a5380fcdb7d2b2253c8f2dbb7d0f50051
EnzoFanAccount <enzobiondo11@outlook.com>
2025-05-27 10:44:45 -0300

    lint

--- browser_use/agent/service.py
+++ browser_use/agent/service.py
@@ -1059,9 +1059,7 @@ class Agent(Generic[Context]):
 		"""Create and store history item"""
 
 		if model_output:
-			interacted_elements = AgentHistory.get_interacted_element(
-				model_output, browser_state_summary.selector_map
-			)
+			interacted_elements = AgentHistory.get_interacted_element(model_output, browser_state_summary.selector_map)
 		else:
 			interacted_elements = [None]
 
