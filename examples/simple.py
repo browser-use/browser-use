af2152d1d70a7f8629ccf70b9ca261715cbc4023
NaveenSandaruwan <157776142+NaveenSandaruwan@users.noreply.github.com>
2025-08-28 10:29:54 +0530

    fix: Update LLM model to ChatGoogle and add asyncio support

--- examples/simple.py
+++ examples/simple.py
@@ -1,8 +1,13 @@
-from browser_use import Agent, ChatOpenAI
+import asyncio
+from py_compile import main
+from browser_use import Agent, ChatOpenAI ,ChatGoogle
 
 agent = Agent(
 	task='Find founders of browser-use',
-	llm=ChatOpenAI(model='gpt-4.1-mini'),
+	llm=ChatGoogle(model='gemini-2.0-flash'),
 )
 
 agent.run_sync()
+
+if __name__ == '__main__':
+    asyncio.run(main())
