2ee9690fe41e0db6fa8d4eaf235b194b067c995b
Eduardo del Palacio Lirola <eddydpyl@gmail.com>
2025-09-15 12:02:36 +0200

    feat: novnc and cookies examples

---
+++ examples/browser/cookies_json.py
@@ -0,0 +1,56 @@
+import asyncio
+import json
+
+from pathlib import Path
+from browser_use.browser.events import BrowserStartEvent
+from browser_use import (
+    BrowserSession,
+    Agent,
+)
+
+
+async def _load_cookies(browser_session: BrowserSession) -> None:
+	"""Load the cookies for the browser session from a JSON file"""
+
+	current_dir = Path(__file__).parent
+	cookies_file = current_dir / "cookies.json"
+
+	if not cookies_file.exists():
+		with open(cookies_file, 'w') as f:
+				json.dump([], f, indent=2)
+
+	cookies = []
+	with open(cookies_file, 'r') as f:
+		cookies = json.load(f)
+
+	event = browser_session.event_bus.dispatch(BrowserStartEvent())
+	await event.event_result(raise_if_any=True, raise_if_none=False)
+	await browser_session._cdp_set_cookies(cookies)
+
+async def _save_cookies(browser_session: BrowserSession) -> None:
+	"""Save the cookies for the browser session to a JSON file"""
+
+	current_dir = Path(__file__).parent
+	cookies_file = current_dir / "cookies.json"
+
+	cookies = await browser_session._cdp_get_cookies()
+
+	with open(cookies_file, 'w') as f:
+		json.dump(cookies, f, indent=2)
+
+async def main():
+	browser_session = BrowserSession()
+	await _load_cookies(browser_session)
+
+	agent = Agent(
+		task='Visit https://duckduckgo.com and search for "browser-use founders"',
+		browser_session=browser_session,
+	)
+
+	await agent.run()
+	await _save_cookies(browser_session)
+	await browser_session.kill()
+
+
+if __name__ == "__main__":
+	asyncio.run(main())
