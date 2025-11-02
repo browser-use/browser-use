6859a7fffa114c4de8dd0a54fcab399f5d94ac67
kalil0321 <kalil.bouzigues@gmail.com>
2025-11-02 03:38:12 +0100

    Add session export options and code validation integration

--- browser_use/code_use/__init__.py
+++ browser_use/code_use/__init__.py
@@ -1,8 +1,14 @@
 """Code-use mode - Jupyter notebook-like code execution for browser automation."""
 
 from browser_use.code_use.namespace import create_namespace
-from browser_use.code_use.notebook_export import export_to_ipynb, session_to_python_script
+from browser_use.code_use.notebook_export import (
+	export_to_ipynb,
+	session_to_playwright_script,
+	session_to_python_script,
+	session_to_typescript_script,
+)
 from browser_use.code_use.service import CodeAgent
+from browser_use.code_use.validator import CodeValidator, ValidationIssue, ValidationResult
 from browser_use.code_use.views import CodeCell, ExecutionStatus, NotebookSession
 
 __all__ = [
@@ -10,6 +16,11 @@ __all__ = [
 	'create_namespace',
 	'export_to_ipynb',
 	'session_to_python_script',
+	'session_to_playwright_script',
+	'session_to_typescript_script',
+	'CodeValidator',
+	'ValidationIssue',
+	'ValidationResult',
 	'CodeCell',
 	'ExecutionStatus',
 	'NotebookSession',
